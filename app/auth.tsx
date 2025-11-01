f9bb116f8215db4e561de5ec6c2a9d69914fcf6a
lalatina11 <rahmadancandra@gmail.com>
2025-11-01 14:47:55 +0700

    chore: new auth page

---
+++ app/auth.tsx
@@ -0,0 +1,50 @@
+import RegisterForm from "@/components/forms/Register";
+import { Box } from "@/components/ui/box";
+import { Card } from "@/components/ui/card";
+import { Heading } from "@/components/ui/heading";
+import { Text } from "@/components/ui/text";
+import { useState } from "react";
+import { KeyboardAvoidingView } from "react-native";
+
+export type AuthPageParams = "login" | "register";
+
+const AuthPage = () => {
+  const [params, setParams] = useState<AuthPageParams>("login");
+
+  const isLoginPage = params === "login";
+
+  return (
+    <KeyboardAvoidingView style={{ flex: 1 }}>
+      <Box className="p-3">
+        <Card size="md" variant="elevated" className="flex flex-col gap-6">
+          <Box>
+            <Heading size="md" className="">
+              {isLoginPage ? "Login" : "Register"}
+            </Heading>
+            <Text size="sm">
+              {isLoginPage
+                ? "Login Into Your Account"
+                : "Register Your Account"}
+            </Text>
+          </Box>
+          <RegisterForm />
+          <Box className="mx-auto flex flex-row gap-1">
+            <Text className="text-sm">
+              {isLoginPage
+                ? "Don't have an account?"
+                : "Already have an account?"}
+            </Text>
+            <Text
+              className="text-sm underline underline-offset-4"
+              onPress={() => setParams(isLoginPage ? "register" : "login")}
+            >
+              {isLoginPage ? "register here" : "login here"}
+            </Text>
+          </Box>
+        </Card>
+      </Box>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default AuthPage;
