74db70bd5abd01640de4959a9e0be128a2d15999
lalatina11 <rahmadancandra@gmail.com>
2025-11-15 11:12:55 +0700

    chore: adding additional page

---
+++ app/(tabs)/setting.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@/components/ui/box";
+import { Text } from "@/components/ui/text";
+import React from "react";
+
+const Setting = () => {
+  return (
+    <Box className="bg-secondary-0 flex-1">
+      <Text>Setting</Text>
+    </Box>
+  );
+};
+
+export default Setting;
