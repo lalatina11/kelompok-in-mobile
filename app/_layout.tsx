a13f59e8d6c37f53782bb1e4b599d48648be12fe
lalatina11 <rahmadancandra@gmail.com>
2025-11-15 10:16:28 +0700

    feat(auth): apply auth guard middleware

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,38 +4,45 @@ import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import "@/global.css";
 import QueryClientProvider from "@/components/providers/QueryClientProvider";
 import useThemeStore from "@/lib/stores/useThemeStore";
+import AuthGuard from "@/components/auth-guard";
 
 export default function RootLayout() {
   const { theme } = useThemeStore();
   return (
     <QueryClientProvider>
       <GluestackUIProvider mode={theme}>
-        <Stack>
-          <Stack.Screen
-            name="index"
-            options={{
-              headerStyle: {
-                backgroundColor: theme === "dark" ? "black" : "white",
-              },
-              headerTitleStyle: { color: theme === "dark" ? "white" : "black" },
-              title: "Home",
-              headerTitleAlign: "center",
-              headerBackVisible: false,
-            }}
-          />
-          <Stack.Screen
-            name="auth"
-            options={{
-              headerStyle: {
-                backgroundColor: theme === "dark" ? "black" : "white",
-              },
-              headerTitleStyle: { color: theme === "dark" ? "white" : "black" },
-              title: "Welcome",
-              headerTitleAlign: "center",
-              headerBackVisible: false,
-            }}
-          />
-        </Stack>
+        <AuthGuard>
+          <Stack>
+            <Stack.Screen
+              name="index"
+              options={{
+                headerStyle: {
+                  backgroundColor: theme === "dark" ? "black" : "white",
+                },
+                headerTitleStyle: {
+                  color: theme === "dark" ? "white" : "black",
+                },
+                title: "Home",
+                headerTitleAlign: "center",
+                headerBackVisible: false,
+              }}
+            />
+            <Stack.Screen
+              name="auth"
+              options={{
+                headerStyle: {
+                  backgroundColor: theme === "dark" ? "black" : "white",
+                },
+                headerTitleStyle: {
+                  color: theme === "dark" ? "white" : "black",
+                },
+                title: "Welcome",
+                headerTitleAlign: "center",
+                headerBackVisible: false,
+              }}
+            />
+          </Stack>
+        </AuthGuard>
       </GluestackUIProvider>
     </QueryClientProvider>
   );
