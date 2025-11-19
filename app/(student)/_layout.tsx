645198354d88cddbeb47650bd2d6d6c0bf8eeb25
lalatina11 <rahmadancandra@gmail.com>
2025-11-19 19:02:22 +0700

    feat: student screen

---
+++ app/(student)/_layout.tsx
@@ -0,0 +1,38 @@
+import useThemeStore from "@/lib/stores/useThemeStore";
+import { Tabs } from "expo-router";
+import { Home, Settings } from "lucide-react-native";
+
+const TabsLayout = () => {
+  const { theme } = useThemeStore();
+  const isDark = theme === "dark";
+  const isRevertColor = isDark ? "white" : "black";
+  const isNotRevertColor = !isDark ? "white" : "black";
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerShown: false,
+          tabBarStyle: { backgroundColor: isNotRevertColor },
+          tabBarActiveBackgroundColor: isRevertColor,
+          tabBarIcon: ({ color }) => <Home color={color} />,
+          tabBarActiveTintColor: isNotRevertColor,
+          title: "Home",
+        }}
+      />
+      <Tabs.Screen
+        name="setting"
+        options={{
+          headerShown: false,
+          tabBarStyle: { backgroundColor: isNotRevertColor },
+          tabBarActiveBackgroundColor: isRevertColor,
+          tabBarIcon: ({ color }) => <Settings color={color} />,
+          tabBarActiveTintColor: isNotRevertColor,
+          title: "Setting",
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
---
+++ app/(student)/setting.tsx
@@ -0,0 +1,7 @@
+import SettingPage from "@/components/setting-page";
+
+const Setting = () => {
+  return <SettingPage />;
+};
+
+export default Setting;
