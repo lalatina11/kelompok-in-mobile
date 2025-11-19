3333a82fc053f8ea13453b32c1d27d86a106416e
lalatina11 <rahmadancandra@gmail.com>
2025-11-19 19:04:47 +0700

    chore: all settings page components are here

---
+++ components/setting-page.tsx
@@ -0,0 +1,12 @@
+import LogoutForm from "./forms/LogoutForm";
+import { Box } from "./ui/box";
+
+const SettingPage = () => {
+  return (
+    <Box className="bg-secondary-0 flex-1 p-2">
+      <LogoutForm />
+    </Box>
+  );
+};
+
+export default SettingPage;
